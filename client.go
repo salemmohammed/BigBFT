de304993d24a7fc60c9d4924753bc020ec4d77af
salemmohammed <salemmoh@buffalo.edu>
2021-03-30 12:50:55 -0400

    httpCode

--- client.go
+++ client.go
@@ -1,26 +1,28 @@
 package BigBFT
+
 import "C"
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"github.com/salemmohammed/BigBFT/lib"
 	"github.com/salemmohammed/BigBFT/log"
-	"bufio"
-	"container/list"
-	"encoding/gob"
-	"errors"
-	"fmt"
 	"io"
-	"net"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
 	"strconv"
-	"strings"
 	"sync"
-	"time"
 )
+
 // Client interface provides get and put for key value store
 type Client interface {
+	GetMUL(Key) ([]Value, []map[string]string)
+	PutMUL(Key, Value) []error
 	Get(Key) (Value, error)
 	Put(Key, Value) error
 }
-// https://www.oreilly.com/content/building-messaging-in-go-network-clients/
+
 // AdminClient interface provides fault injection opeartion
 type AdminClient interface {
 	Consensus(Key) bool
@@ -28,38 +30,27 @@ type AdminClient interface {
 	Drop(ID, ID, int)
 	Partition(int, ...ID)
 }
-type client interface {
-	processInfo(line string)
-	processMsg(subj string, reply string, sid int, payload []byte)
-	processPing()
-	processPong()
-	processErr(msg string)
-	processOpErr(err error)
-}
+
 // HTTPClient inplements Client interface with REST API
 type HTTPClient struct {
 	Addrs  map[ID]string
+	HTTP   map[ID]string
 	ID     ID  // client id use the same id as servers in local site
 	N      int // total number of nodes
 	LocalN int // number of nodes in local zone
+
 	CID int // command id
-	conn net.Conn
-	data   chan []byte
-	w    *bufio.Writer // ??
-	sync.Mutex
-	reqSent *list.List  // list of requests sent, waiting for response
-	mutex   sync.Mutex
-	pending map[uint64]*Request
+	*http.Client
 }
+
 // NewHTTPClient creates a new Client from config
-// New client
 func NewHTTPClient(id ID) *HTTPClient {
-	log.Debugf("I received request from clinet to start NewHTTPClient")
 	c := &HTTPClient{
 		ID:     id,
 		N:      len(config.Addrs),
 		Addrs:  config.Addrs,
-		//HTTP:   config.HTTPAddrs,
+		HTTP:   config.HTTPAddrs,
+		Client: &http.Client{},
 	}
 	if id != "" {
 		i := 0
@@ -73,6 +64,7 @@ func NewHTTPClient(id ID) *HTTPClient {
 
 	return c
 }
+
 // Get gets value of given key (use REST)
 // Default implementation of Client interface
 func (c *HTTPClient) Get(key Key) (Value, error) {
@@ -80,115 +72,186 @@ func (c *HTTPClient) Get(key Key) (Value, error) {
 	v, _, err := c.RESTGet(c.ID, key)
 	return v, err
 }
+
 // Put puts new key value pair and return previous value (use REST)
 // Default implementation of Client interface
 func (c *HTTPClient) Put(key Key, value Value) error {
-	log.Debugf("Write Function in Client.go")
 	c.CID++
 	_, _, err := c.RESTPut(c.ID, key, value)
 	return err
 }
+
+func (c *HTTPClient) GetMUL(key Key) ([]Value, []map[string]string) {
+	log.Debugf("<--------------GetMUL-------------->")
+	valueChannel := make(chan Value, len(c.HTTP))
+	MetaChannel := make(chan map[string]string,len(c.HTTP))
+	//log.Debugf("valueChannel_0_0=%v",<- valueChannel)
+	i := 0
+	for id := range c.HTTP {
+		go func(id ID) {
+			c.CID++
+			v, meta, err := c.rest(id, key, nil,c.CID)
+			if err != nil {
+				log.Error(err)
+				return
+			}
+			valueChannel <- v
+			MetaChannel <- meta
+		}(id)
+		i++
+	}
+	values := make([]Value,0)
+	metas := make([]map[string]string,0)
+	for ; i>0; i--{
+		values = append(values, <-valueChannel)
+		metas = append(metas, <-MetaChannel)
+	}
+	//log.Debugf("values %v ", values)
+	//log.Debugf("metas %v ", metas)
+	return values, metas
+}
+
+// Put puts new key value pair and return previous value (use REST)
+// Default implementation of Client interface
+func (c *HTTPClient) PutMUL(key Key, value Value) []error {
+	log.Debugf("<----------------PutMUL---------------->")
+	i := 0
+	errs := make(chan error,len(c.HTTP))
+	for id := range c.HTTP {
+		//log.Debugf("id=%v",id)
+		go func(id ID) {
+			c.CID++
+			_, _, err := c.rest(id, key, value,c.CID)
+			if err != nil {
+				log.Error(err)
+				return
+			}
+			errs <- err
+		}(id)
+		i++
+	}
+	errors := make([]error,0)
+	for ; i>0; i-- {
+		errors = append(errors, <-errs)
+	}
+	//log.Debugf("errors %v ", errors)
+	return errors
+}
+
 func (c *HTTPClient) GetURL(id ID, key Key) string {
 	if id == "" {
-		for id = range c.Addrs {
+		for id = range c.HTTP {
 			if c.ID == "" || id.Zone() == c.ID.Zone() {
 				break
 			}
 		}
 	}
-	return c.Addrs[id] + "/" + strconv.Itoa(int(key))
+	return c.HTTP[id] + "/" + strconv.Itoa(int(key))
 }
-// Close terminates a connection to NATS.
-func (c *HTTPClient) Close() {
-	c.Lock()
-	defer c.Unlock()
-	c.conn.Close()
-}
+
 // rest accesses server's REST API with url = http://ip:port/key
 // if value == nil, it's a read
-func (c *HTTPClient) Send(id ID, key Key, value Value) (Value, map[string]string, error) {
-	log.Debugf("rest Function in Client.go")
-	log.Debugf("c.Addrs[c.ID] = %v", c.Addrs[c.ID])
-	// dial the server
-	conn, err := net.Dial("tcp", c.Addrs[c.ID])
+func (c *HTTPClient) rest(id ID, key Key, value Value,count int) (Value, map[string]string, error) {
+	// get url
+	//log.Debugf("I am in rest")
+	url := c.GetURL(id, key)
+
+	method := http.MethodGet
+	var body io.Reader
+	if value != nil {
+		method = http.MethodPut
+		body = bytes.NewBuffer(value)
+		//log.Debugf("method=%v",method)
+	}
+	//log.Debugf("URL %v", url)
+	req, err := http.NewRequest(method, url, body)
+	//log.Debugf("req=%v",req)
 	if err != nil {
+		log.Debugf("we cannot create a request")
+		log.Error(err)
 		return nil, nil, err
 	}
-	//defer conn.Close()
-	log.Debugf("Connected to server...")
-	//encoder := gob.NewEncoder(buffer)
+	//log.Debugf("HTTPClientID=%v",HTTPClientID)
+	//log.Debugf("HTTPCommandID=%v",HTTPCommandID)
+	//log.Debugf("c.ID=%v",c.ID)
+	//log.Debugf("CID=%v",count)
 
-	c.mutex.Lock()
-	encoder := gob.NewEncoder(conn)
-	c.CID++
+	req.Header.Set(HTTPClientID, string(id))
+	req.Header.Set(HTTPCommandID, strconv.Itoa(count))
+	// r.Header.Set(HTTPTimestamp, strconv.FormatInt(time.Now().UnixNano(), 10))
 
+	rep, err := c.Client.Do(req)
+	//log.Debugf("c.Client.Do(req)=%v",rep)
+	if err != nil {
+		log.Error(err)
+		return nil, nil, err
+	}
+
+
+	defer rep.Body.Close()
+
+	// get headers
+	metadata := make(map[string]string)
+	for k := range rep.Header {
+		metadata[k] = rep.Header.Get(k)
+	}
+	//log.Debugf("metadata=%v",metadata)
+	//log.Debugf("rep.StatusCode=%v",rep.StatusCode)
+
+	if rep.StatusCode == http.StatusOK {
+		b, err := ioutil.ReadAll(rep.Body)
+		if err != nil {
+			log.Error(err)
+			return nil, metadata, err
+		}
+
+		if value == nil{
+			log.Debugf("node=%v type=%s key=%v value=%x", id, method, key, Value(b))
+		} else {
+			log.Debugf("node=%v type=%s key=%v value=%x", id, method, key, value)
+		}
+		return Value(b), metadata, nil
+	}
+
+	// http call failed
+	dump, _ := httputil.DumpResponse(rep, true)
+	log.Debugf("%q", dump)
+	return nil, metadata, errors.New(rep.Status)
+}
+
+// RESTGet issues a http call to node and return value and headers
+func (c *HTTPClient) RESTGet(id ID, key Key) (Value, map[string]string, error) {
+	return c.rest(id, key, nil,c.CID)
+}
+
+// RESTPut puts new value as http.request body and return previous value
+func (c *HTTPClient) RESTPut(id ID, key Key, value Value) (Value, map[string]string, error) {
+	return c.rest(id, key, value,c.CID)
+}
+
+func (c *HTTPClient) json(id ID, key Key, value Value) (Value, error) {
+	url := c.HTTP[id]
 	cmd := Command{
 		Key:       key,
 		Value:     value,
 		ClientID:  c.ID,
 		CommandID: c.CID,
 	}
-
-	req := Request{
-		Command:    cmd,
-		Properties: make(map[string]string),
-		Timestamp:  time.Now().UnixNano(),
-		NodeID:     id,
-		c:          make(chan Reply, 1),
-		Done:       nil,
-	}
-
-	err = encoder.Encode(req)
+	data, err := json.Marshal(cmd)
+	res, err := c.Client.Post(url, "json", bytes.NewBuffer(data))
 	if err != nil {
-		log.Fatal("encode error:", err)
+		log.Error(err)
+		return nil, err
 	}
-	c.mutex.Unlock()
-
-	select {
-	case <-req.Done:
-	case <-time.After(2 * time.Second):
-		req.Error = errors.New("request timeout")
+	defer res.Body.Close()
+	if res.StatusCode == http.StatusOK {
+		b, _ := ioutil.ReadAll(res.Body)
+		log.Debugf("key=%v value=%x", key, Value(b))
+		return Value(b), nil
 	}
-	if req.Error != nil {
-		return nil,nil, req.Error
-	}
-	return req.Command.Value, nil, nil
-}
-
-// RESTGet issues a http call to node and return value and headers
-func (c *HTTPClient) RESTGet(id ID, key Key) (Value, map[string]string, error) {
-	return c.Send(id, key, nil)
-}
-
-// RESTPut puts new value as http.request body and return previous value
-func (c *HTTPClient) RESTPut(id ID, key Key, value Value) (Value, map[string]string, error) {
-	log.Debugf("RESTPut Function in Client.go")
-	return c.Send(id, key, value)
-}
-
-func (c *HTTPClient) json(id ID, key Key, value Value) (Value, error) {
-	////url := c.Addrs[id]
-	//cmd := Command{
-	//	Key:       key,
-	//	Value:     value,
-	//	ClientID:  c.ID,
-	//	CommandID: c.CID,
-	//}
-	////data, err := json.Marshal(cmd)
-	//res, err := c.Client.Post(url, "json", bytes.NewBuffer(data))
-	//if err != nil {
-	//	log.Error(err)
-	//	return nil, err
-	//}
-	//defer res.Body.Close()
-	//if res.StatusCode == http.StatusOK {
-	//	b, _ := ioutil.ReadAll(res.Body)
-	//	log.Debugf("key=%v value=%x", key, Value(b))
-	//	return Value(b), nil
-	//}
-	//dump, _ := httputil.DumpResponse(res, true)
-	//log.Debugf("%q", dump)
-	return nil, nil
+	dump, _ := httputil.DumpResponse(res, true)
+	log.Debugf("%q", dump)
+	return nil, errors.New(res.Status)
 }
 
 // JSONGet posts get request in json format to server url
@@ -211,9 +274,9 @@ func (c *HTTPClient) MultiGet(n int, key Key) ([]Value, []map[string]string) {
 	valueC := make(chan Value)
 	metaC := make(chan map[string]string)
 	i := 0
-	for id := range c.Addrs {
+	for id := range c.HTTP {
 		go func(id ID) {
-			v, meta, err := c.Send(id, key, nil)
+			v, meta, err := c.rest(id, key, nil,c.CID)
 			if err != nil {
 				log.Error(err)
 				return
@@ -240,7 +303,7 @@ func (c *HTTPClient) LocalQuorumGet(key Key) ([]Value, []map[string]string) {
 	valueC := make(chan Value)
 	metaC := make(chan map[string]string)
 	i := 0
-	for id := range c.Addrs {
+	for id := range c.HTTP {
 		if c.ID.Zone() != id.Zone() {
 			continue
 		}
@@ -249,7 +312,7 @@ func (c *HTTPClient) LocalQuorumGet(key Key) ([]Value, []map[string]string) {
 			break
 		}
 		go func(id ID) {
-			v, meta, err := c.Send(id, key, nil)
+			v, meta, err := c.rest(id, key, nil,c.CID)
 			if err != nil {
 				log.Error(err)
 				return
@@ -273,14 +336,14 @@ func (c *HTTPClient) LocalQuorumGet(key Key) ([]Value, []map[string]string) {
 func (c *HTTPClient) QuorumPut(key Key, value Value) {
 	var wait sync.WaitGroup
 	i := 0
-	for id := range c.Addrs {
+	for id := range c.HTTP {
 		i++
 		if i > c.N/2 {
 			break
 		}
 		wait.Add(1)
 		go func(id ID) {
-			c.Send(id, key, value)
+			c.rest(id, key, value,c.CID)
 			wait.Done()
 		}(id)
 	}
@@ -290,6 +353,27 @@ func (c *HTTPClient) QuorumPut(key Key, value Value) {
 // Consensus collects /history/key from every node and compare their values
 func (c *HTTPClient) Consensus(k Key) bool {
 	h := make(map[ID][]Value)
+	for id, url := range c.HTTP {
+		h[id] = make([]Value, 0)
+		r, err := c.Client.Get(url + "/history?key=" + strconv.Itoa(int(k)))
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+		holder := h[id]
+		err = json.Unmarshal(b, &holder)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+		h[id] = holder
+		log.Debugf("node=%v key=%v h=%v", id, k, holder)
+	}
 	n := 0
 	for _, v := range h {
 		if len(v) > n {
@@ -298,7 +382,7 @@ func (c *HTTPClient) Consensus(k Key) bool {
 	}
 	for i := 0; i < n; i++ {
 		set := make(map[string]struct{})
-		for id := range c.Addrs {
+		for id := range c.HTTP {
 			if len(h[id]) > i {
 				set[string(h[id][i])] = struct{}{}
 			}
@@ -313,10 +397,24 @@ func (c *HTTPClient) Consensus(k Key) bool {
 // Crash stops the node for t seconds then recover
 // node crash forever if t < 0
 func (c *HTTPClient) Crash(id ID, t int) {
+	url := c.HTTP[id] + "/crash?t=" + strconv.Itoa(t)
+	r, err := c.Client.Get(url)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	r.Body.Close()
 }
 
 // Drop drops every message send for t seconds
 func (c *HTTPClient) Drop(from, to ID, t int) {
+	url := c.HTTP[from] + "/drop?id=" + string(to) + "&t=" + strconv.Itoa(t)
+	r, err := c.Client.Get(url)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	r.Body.Close()
 }
 
 // Partition cuts the network between nodes for t seconds
@@ -327,79 +425,9 @@ func (c *HTTPClient) Partition(t int, nodes ...ID) {
 	}
 	for from := range c.Addrs {
 		if !s.Has(from) {
-		}
-	}
-}
-
-// readLoop is ran as a goroutine and processes commands
-// sent by the server.
-func readLoop(c client, conn net.Conn) {
-	r := bufio.NewReader(conn)
-
-	for {
-		line, err := r.ReadString('\n')
-		if err != nil {
-			c.processOpErr(err)
-			return
-		}
-		args := strings.SplitN(line, " ", 2)
-		if len(args) < 1 {
-			c.processOpErr(errors.New("Error: malformed control line"))
-			return
-		}
-
-		op := strings.TrimSpace(args[0])
-		switch op {
-		case "MSG":
-			var subject, reply string
-			var sid, size int
-
-			n := strings.Count(args[1], " ")
-			switch n {
-			case 2:
-				// No reply is expected in this case (message is just broadcast).
-				// MSG foo 1 3\r\n
-				// bar\r\n
-				_, err := fmt.Sscanf(args[1], "%s %d %d",
-					&subject, &sid, &size)
-				if err != nil {
-					c.processOpErr(err)
-					return
-				}
-			case 3:
-				// Reply is expected in this case (a request).
-				// MSG foo 1 bar 4\r\n
-				// quux\r\n
-				_, err := fmt.Sscanf(args[1], "%s %d %s %d",
-					&subject, &sid, &reply, &size)
-				if err != nil {
-					c.processOpErr(err)
-					return
-				}
-			default:
-				c.processOpErr(errors.New("nats: bad control line"))
-				return
+			for _, to := range nodes {
+				c.Drop(from, to, t)
 			}
-
-			// Prepare buffer for the payload
-			payload := make([]byte, size)
-			_, err = io.ReadFull(r, payload)
-			if err != nil {
-				c.processOpErr(err)
-				return
-			}
-			// In the two-argument case, the reply below is null.
-			c.processMsg(subject, reply, sid, payload)
-		case "INFO":
-			c.processInfo(args[1])
-		case "PING":
-			c.processPing()
-		case "PONG":
-			c.processPong()
-		case "+OK":
-			// Do nothing.
-		case "-ERR":
-			c.processErr(args[1])
 		}
 	}
 }
