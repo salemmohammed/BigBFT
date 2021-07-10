14ec090635e846a5a4df4abbb6ac06d4b8fd736e
salemmohammed <salemmoh@buffalo.edu>
2021-07-10 04:32:43 +0300

    ips

--- bin/del.sh
+++ bin/del.sh
@@ -7,4 +7,13 @@ kill -9 $(lsof -i:1739 -t)
 kill -9 $(lsof -i:1740 -t)
 kill -9 $(lsof -i:1741 -t)
 kill -9 $(lsof -i:1742 -t)
+kill -9 $(lsof -i:1743 -t)
+kill -9 $(lsof -i:1744 -t)
+kill -9 $(lsof -i:1745 -t)
+kill -9 $(lsof -i:1746 -t)
+kill -9 $(lsof -i:1747 -t)
+kill -9 $(lsof -i:1748 -t)
+kill -9 $(lsof -i:1749 -t)
+kill -9 $(lsof -i:1750 -t)
+
 rm client* server* cmd history* latency
