3ac7ae0212d8d15927764ec0f94ac7d391a13250
salemmohammed <salemmoh@buffalo.edu>
2021-07-11 21:12:13 +0300

    size

--- benchmark.go
+++ benchmark.go
@@ -65,7 +65,7 @@ func DefaultBConfig() Bconfig {
 		ZipfianS:             2,
 		ZipfianV:             1,
 		Lambda:               0.01,
-		Size:				  512,
+		Size:				  256,
 	}
 }
 // Benchmark is benchmarking tool that generates workload and collects operation history and latency
